2911cec0915510cf55b974587ccb2431638eb799
jiyoung choi <sarahchoi.webdeveloper@outlook.com>
2024-05-25 01:41:03 +1000

    update timelimit

--- src/app/api/chat/route.ts
+++ src/app/api/chat/route.ts
@@ -1,6 +1,6 @@
 import { NextRequest, NextResponse } from 'next/server';
 import openai from '../../../lib/openai';
-
+export const maxDuration = 40; // This function can run for a maximum of 5 seconds
 export async function POST(req: NextRequest) {
   try {
     const { prompt } = await req.json();
